c8a87711c28453af8d122f438e45d1acae217636
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2020-05-30 15:32:43 -0300

    Classe estática Estratégia

---
+++ PI3 - Fukushima/Estrategia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI3___Fukushima
+{
+    static class Estrategia
+    {
+        public static void MenorChao()
+        {
+
+        }
+        public static void MaiorModelo()
+        {
+
+        }
+        public static void MenorModelo()
+        {
+
+        }
+        public static void CompletaModelo()
+        {
+
+        }
+        public static void PreencheComCentro()
+        {
+
+        }
+        public static void MenorCentro()
+        {
+
+        }
+        public static void MaiorCentro()
+        {
+
+        }
+    }
+}
