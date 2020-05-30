24e631c818ffd6603a79f3b78c1a3c1b0a712a89
GustavoYamauchi <gyxgames31@gmail.com>
2020-05-30 15:29:16 -0300

    Criando Classes Compra e Jogada

---
+++ PI3 - Fukushima/Jogada.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI3___Fukushima
+{
+    class Jogada : Azulejo
+    {
+        public int IdFabrica { get; set; }
+
+    }
+}
